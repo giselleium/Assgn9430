5c16e3a41bf0f7c7c187956149abead92737171c
Quinn Peterson <qap2001@gmail.com>
2022-11-30 11:49:55 -0800

    refactor of extendEnvMult, and interp to use match

--- asgn9.jl
+++ asgn9.jl
@@ -1,4 +1,7 @@
 using Test
+# import Pkg;
+# Pkg.add("Match");
+using Match
 
 begin
     # ExprC structs 
@@ -90,14 +93,6 @@ topEnv = Environment([
     Bind("error", PrimV(["v"], ErrC(IdC("v"))))
 ])
 
-
-
-# takes a binding and an environment and returns the given environment
-# with the binding added
-
-extendEnv(b::Bind, env::Environment) = push!(env, b)
-
-
 # takes a value and returns the serialization of the value
 function serialize(v)
     if (typeof(v) == NumV)
@@ -158,48 +153,9 @@ end
 @test interpPrim("<=", NumV(4), NumV(10)) == BoolV(true)
 @test interpPrim("equal?", NumV(4), NumV(10)) == BoolV(false)
 
-# takes a JYSS5 expression and an environment
-# and returns the value the expression evaluates to
-function interp(e::ExprC, env::Environment)
-    if (typeof(e) == NumC)
-        NumV(e.n)
-    elseif (typeof(e) == StrC)
-        StrV(e.s)
-    elseif (typeof(e) == IdC)
-        lookup(e.i, env)
-    elseif (typeof(e) == AppC)
-
-
-        # elseif (typeof(e) == PrimC)
-        # elseif (typeof(e) == ErrC)
-        # elseif (typeof(e) == IfC)
-        # elseif (typeof(e) == LamC)
-        # else
-    end
-end
-@test interp(NumC(10), topEnv) == NumV(10)
-@test interp(StrC("10"), topEnv) == StrV("10")
-
-
-# takes a list of closure arguments, a list of application arguments,
-# a closure environment, and an application environment and adds the bindings
-# to the closure environment
-
-# Use Strings for Symbols? Have not finished this method
-
-extendEnvMult(cArgs::Array{String}, fArgs::Array{ExprC}, cEnv::Environment, fEnv::Environment) =
-    if isempty(cArgs)
-        "extend-env-mult too many arguments provided (JYSS)"
-    elseif isempty(fArgs)
-        "extend-env-mult too many arguments provided (JYSS)"
-    else
-        extendEnvMult() # haven't implemented yet
-    end
-
-#takes a symbol and an environment and returns the value
-#of the binding that has that symbol, if there is one
-
-lookup(target::String, env::Vector{Bind}) =
+# takes a symbol and an environment and returns the value
+# of the binding that has that symbol, if there is one
+function lookup(target::String, env::Vector{Bind})
     if isempty(env)
         println("name not found")
     else
@@ -210,10 +166,64 @@ lookup(target::String, env::Vector{Bind}) =
             lookup(target, env)
         end
     end
-
+end
 @test lookup("a", [Bind("b", "first value"), Bind("b", "second value"), Bind("a", "third value")]) == "third value"
 @test lookup("b", [Bind("a", "first value"), Bind("b", "second value"), Bind("a", "third value")]) == "second value"
 @test lookup("c", [Bind("b", "first value"), Bind("a", "second value"), Bind("c", "third value")]) == "third value"
 
+# takes a JYSS5 expression and an environment
+# and returns the value the expression evaluates to
+function interp(e::ExprC, env::Environment)
+    @match e begin
+        NumC(n) => NumV(n)
+        StrC(s) => StrV(s)
+        IdC(i) => lookup(string(i), env.b)
+
+    end
+
+    # if (typeof(e) == NumC)
+    #     NumV(e.n)
+    # elseif (typeof(e) == StrC)
+    #     StrV(e.s)
+    # elseif (typeof(e) == IdC)
+    #     lookup(string(e.i), env.b)
+    # elseif (typeof(e) == AppC)
+    #     res = interp(e.app, env)
+
+    #     if (typeof(res) == ClosV)
+    #         nextRes = interp(res.body, ExtendEnvMult(res.args, e.a (closV-env f-value) env)))
+
+    #     end
+
+    #     # elseif (typeof(e) == PrimC)
+    #     # elseif (typeof(e) == ErrC)
+    #     # elseif (typeof(e) == IfC)
+    #     # elseif (typeof(e) == LamC)
+    #     # else
+    # end
+end
+@test interp(NumC(10), topEnv) == NumV(10)
+@test interp(StrC("10"), topEnv) == StrV("10")
+@test interp(IdC("false"), topEnv) == BoolV(false)
 
 
+# takes a binding and an environment and returns the given environment
+# with the binding added
+extendEnv(b::Bind, env::Environment) = push!(env, b)
+
+# takes a list of closure arguments, a list of application arguments,
+# a closure environment, and an application environment and adds the bindings
+# to the closure environment
+# Use Strings for Symbols? Have not finished this method
+function extendEnvMult(cArgs::Array{String}, fArgs::Array{ExprC}, cEnv::Environment, fEnv::Environment)
+    if (isempty(cArgs) && isempty(fArgs))
+        cEnv
+    elseif isempty(cArgs)
+        throw(DomainError(cArgs, "extend-env-mult too many arguments provided (JYSS5)"))
+    elseif isempty(fArgs)
+        throw(DomainError(fArgs, "extend-env-mult too many arguments provided (JYSS)"))
+    else
+        temp = extendEnv(Bind(cArgs[1], interp(fArgs[1], fEnv)), cEnv)
+        # extendEnvMult(cArgs[2:...], fArgs[2:...], temp, fEnv)
+    end
+end
